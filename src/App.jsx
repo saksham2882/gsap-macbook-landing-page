8c63568e0080238cbd730445361afe753bc171e7
Saksham Agrahari <agrahari0899@gmail.com>
2025-11-17 20:07:40 +0530

    feat: Implement Showcase Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import NavBar from "./components/NavBar";
 import ProductViewer from "./components/ProductViewer";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
+import Showcase from "./components/Showcase";
 
 // Register the ScrollTrigger plugin (globally available)
 gsap.registerPlugin(ScrollTrigger);  
@@ -13,6 +14,7 @@ const App = () => {
       <NavBar />
       <Hero />
       <ProductViewer />
+      <Showcase />
     </main>
   );
 };
